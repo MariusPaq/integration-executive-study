8a4b5526b5c5cf2e2571b6a0057dabf84bf21851
MariusPaq <marius.paquet@gmail.com>
2020-09-22 14:28:40 +0200

    create footer

--- footer.php
+++ footer.php
@@ -1,4 +1,30 @@
-    
+  <div class="row footer">
+      <div class="col txt-footer">
+          <img src="img/ETI-Logo.png" alt="logo">
+          <p>Concord Royal Court (3rd floor)</p>
+          <p>Dhanmondi, Dhaka 1209, Bangladesh.</p>
+          <p id="copyright">© 2020 Executive Trade International.</p>
+      </div>
+      <div class="col link-footer">
+          <a href="#">About Us</a><br><br>
+          <a href="#">Responsibilties</a><br><br>
+          <a href="#">Our services</a><br><br>
+          <a href="#">Contact</a><br><br>
+      </div>
+      <div class="col link-footer">
+          <a href="#">Disclaimer</a><br><br>
+          <a href="#">testimonials</a><br><br>
+          <a href="#">Privacy Policy</a><br><br>
+          <a href="#">Terms of Service</a><br><br>
+      </div>
+      <div class="col reseaux-footer">
+          <div class="resauxFooter">
+              <img src="img/icon-social-facebook.png" alt="facebook">
+              <img src="img/icon-social-tw.png" alt="twiter">
+              <img src="img/icon-social-insta.png" alt="insta">
+          </div>
+      </div>
+  </div>
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
